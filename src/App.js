08de056fabec2757e9d2345378023175fd9880aa
NaamaL <bunvin@gmail.com>
2022-05-30 20:31:03 +0300

    Added Routes with switch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+//import pages
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About';
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        <h1>My Articles</h1>
-      </nav>
+      <BrowserRouter>
+        <nav>
+          <h1>My Articles</h1>
+        </nav>
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
