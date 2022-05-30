5576637a2b742803dd3638649db853f9e10120e8
NaamaL <bunvin@gmail.com>
2022-05-30 20:55:08 +0300

    NavLinks and a.active class style
    first Routs using NavLinks now just updates the component and not the whole page each selection (like in <a>)
    nav bar now have active selected page with new style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom'
 //import pages
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import About from './pages/About';
+
 
 function App() {
   return (
@@ -11,6 +12,11 @@ function App() {
       <BrowserRouter>
         <nav>
           <h1>My Articles</h1>
+          {/* NavLink changes class to active when clicked on
+          External links use the <a> */}
+          <NavLink exact to="/">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
         </nav>
 
         <Switch>
